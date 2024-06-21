733c9da9e7dd6abaca2d532e9f28e82e6a765445
AndresDiazPinto <andresvis565@gmail.com>
2024-06-20 20:08:50 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -135,11 +135,11 @@ export default {
         titulo: 'Descargar PDF',
         download: 'downloads/722103_CF02_DU.zip',
       },
-      //{
-      //icono: 'fas fa-download',
-      //titulo: 'Descargar material',
-      //download: 'downloads/material.zip',
-      //},
+      {
+        icono: 'fas fa-download',
+        titulo: 'Descargar material',
+        download: 'downloads/material.zip',
+      },
       {
         icono: 'far fa-registered',
         titulo: 'Créditos',
